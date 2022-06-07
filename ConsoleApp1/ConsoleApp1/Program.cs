59ebd33e1385c69abdf9fdbaf84126f8f97b9c92
e11eeveen <107028652+e11eeveen@users.noreply.github.com>
2022-06-07 14:37:51 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿//Код для первого задания
+Console.WriteLine("Введите длину ребра");
+double a = double.Parse(Console.ReadLine());
+double V = Math.Pow(a, 3);
+double S = a * a * 6;
+Console.WriteLine($"{V}, {S}");
