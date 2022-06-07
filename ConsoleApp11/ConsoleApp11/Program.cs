43dbb179d66782219087c1a91a9caf6ff3021f4a
e11eeveen <107028652+e11eeveen@users.noreply.github.com>
2022-06-07 14:52:12 +0700

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,8 @@
+﻿//Код для пятого задания без вариантов
+Console.WriteLine("Какой год: ");
+int x = int.Parse(Console.ReadLine());
+for (int i = 1; i <= 12; i++)
+{
+    DateTime d = new DateTime(x, i, DateTime.DaysInMonth(x, i));
+    Console.WriteLine($"{d.Month} месяц: {d.Day - (7 + (int)d.DayOfWeek - 4) % 7}, четверг.");
+}
