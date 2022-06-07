db1fe1e8f638b3b22ea8949f8f1c55a96cbea85c
e11eeveen <107028652+e11eeveen@users.noreply.github.com>
2022-06-07 14:48:32 +0700

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,36 @@
+﻿//Код для первого задания без вариантов
+using System.Globalization;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime dt;
+        Console.WriteLine("Введите день");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите месяц");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите год");
+        int c = int.Parse(Console.ReadLine());
+        string date = $"{c}, {b}, {a}";
+        int countDaysOfEndOfYear;
+        GregorianCalendar calendar = new GregorianCalendar();
+        if (DateTime.TryParse(date, out dt))
+        {
+            Console.WriteLine(calendar.GetDayOfYear(dt));
+            if (calendar.IsLeapYear(c))
+            {
+                countDaysOfEndOfYear = 366 - calendar.GetDayOfYear(dt);
+
+            }
+            else
+            {
+                countDaysOfEndOfYear = 365 - calendar.GetDayOfYear(dt);
+            }
+            Console.WriteLine(countDaysOfEndOfYear);
+        }
+        else
+        {
+            Console.WriteLine("Неверная дата ");
+        }
+    }
+}
