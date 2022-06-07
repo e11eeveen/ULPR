391ad14e5d1e6416e432d022f5d8de07ae025f3d
e11eeveen <107028652+e11eeveen@users.noreply.github.com>
2022-06-07 14:39:12 +0700

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Код для второго задания
+Console.WriteLine("Введите различные x и y соответственно");
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+double polsum = (x + y) / 2;
+double proizv = x * y * 2;
+if (x > y)
+{
+    x = proizv;
+    y = polsum;
+}
+else
+{
+    x = polsum;
+    y = proizv;
+}
+Console.WriteLine($"x равен {x}; y равен {y}");
