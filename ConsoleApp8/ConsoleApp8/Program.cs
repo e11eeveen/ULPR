361c00a0b6e47578bc28fc7a9a6061b33513b3e2
e11eeveen <107028652+e11eeveen@users.noreply.github.com>
2022-06-07 14:49:14 +0700

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,11 @@
+﻿//Код для второго задания без вариантов
+Console.Write("Год: ");
+int c = int.Parse(Console.ReadLine());
+Console.Write("Месяц: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("День: ");
+int a = int.Parse(Console.ReadLine());
+var dY = new DateTime(c, b, a);
+var dG = dY.AddDays((c / 100) - ((c / 100) / 4) - 2);
+Console.WriteLine($"По юлианскому календарю – {dY}");
+Console.WriteLine($"По григорианскому календарю – {dG}");
