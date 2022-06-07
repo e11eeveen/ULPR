af5c1e4ab80d23c9d1a01274d06b7ff7e1fac2d8
e11eeveen <107028652+e11eeveen@users.noreply.github.com>
2022-06-07 14:34:43 +0700

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿Random gen = new Random();
+Console.WriteLine("Введите количество элементов: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+int sum = 0;
+for (int j = 0; j < n; j++)
+{
+    array[j] = gen.Next(0, 15);
+    Console.WriteLine(array[j]);
+}
+for (int i = 0; i < n; i++)
+{
+    if (array[i] % 3 == 0)
+    {
+        if (array[i] % 7 != 0)
+        {
+            sum = sum + 1;
+        }
+    }
+}
+Console.WriteLine("Сумма: ");
+Console.WriteLine(sum);
