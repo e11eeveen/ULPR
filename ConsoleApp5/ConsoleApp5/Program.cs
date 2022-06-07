07d9ebaebda6407f47bb517d7ecf8475cd41a1e9
e11eeveen <107028652+e11eeveen@users.noreply.github.com>
2022-06-07 14:41:22 +0700

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿//Код для пятого задания
+int a;
+float S = 0;
+int n = 0;
+Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out a);
+while (a > 0)
+{
+    S = S + a;
+    ++n;
+    Console.WriteLine("Введите следующее число");
+    int.TryParse(Console.ReadLine(), out a);
+}
+Console.WriteLine($"Ответ {S / n} ");
+return 0;
