5e8a26416b1f4f3dd93a0e9ab0ff6cb88ae09363
e11eeveen <107028652+e11eeveen@users.noreply.github.com>
2022-06-07 14:41:00 +0700

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Код для четвертого задания
+int n, четные = 0, нечетные = 0;
+Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out n);
+n *= 10;
+for (int i = 0; i < n; i++)
+{
+    n /= 10;
+    if ((n % 10) % 2 == 0) четные++;
+    else нечетные++;
+
+}
+Console.WriteLine($"четных {четные}, нечетных {нечетные} ");
+Console.ReadKey(true);
